11960d680950fce8a16909cc24f2833ecffcaf74
Jeremy Glebe <jeremyglebe@gmail.com>
2019-01-24 11:11:38 -0600

    tutoring about c#

---
+++ tutoring/1_24_csharp/my_application/my_application/NumberGameForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace my_application
+{
+    partial class NumberGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.hitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(371, 132);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(58, 67);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "0";
+            // 
+            // hitButton
+            // 
+            this.hitButton.Location = new System.Drawing.Point(179, 344);
+            this.hitButton.Name = "hitButton";
+            this.hitButton.Size = new System.Drawing.Size(445, 98);
+            this.hitButton.TabIndex = 1;
+            this.hitButton.Text = "Hit";
+            this.hitButton.UseVisualStyleBackColor = true;
+            this.hitButton.Click += new System.EventHandler(this.hitButton_Click);
+            // 
+            // NumberGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.hitButton);
+            this.Controls.Add(this.scoreLabel);
+            this.Name = "NumberGameForm";
+            this.Text = "The Ultimate Number Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Button hitButton;
+    }
+}
+
---
+++ tutoring/1_24_csharp/my_application/my_application/NumberGameForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_application
+{
+    public partial class NumberGameForm : Form
+    {
+        public int score;
+
+        public NumberGameForm()
+        {
+            InitializeComponent();
+            score = 0;
+        }
+
+        private void hitButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            score += (rnd.Next() % 11) + 1;
+            if (score <= 21)
+            {
+                scoreLabel.Text = score.ToString();
+            }
+            else
+            {
+                scoreLabel.Text = "Too high!";
+            }
+        }
+    }
+}
